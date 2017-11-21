561539972cc307a4cb3a84519b41fb3722b2b3d8
Aurelien Broszniowski <aurelien@jsoft.biz>
2017-11-21 17:34:14 +0100

    added integration test module

--- integration-test/src/test/java/com/terracottatech/qa/angela/MultistripesTest.java
+++ integration-test/src/test/java/com/terracottatech/qa/angela/MultistripesTest.java
@@ -1,17 +1,16 @@
 package com.terracottatech.qa.angela;
 
-import org.apache.ignite.lang.IgniteRunnable;
 import org.junit.Test;
 
-import com.terracottatech.qa.angela.common.distribution.Distribution;
 import com.terracottatech.qa.angela.client.TsaControl;
+import com.terracottatech.qa.angela.common.distribution.Distribution;
 import com.terracottatech.qa.angela.common.tcconfig.License;
 import com.terracottatech.qa.angela.common.tcconfig.TcConfig;
 import com.terracottatech.qa.angela.common.topology.LicenseType;
 import com.terracottatech.qa.angela.common.topology.PackageType;
 import com.terracottatech.qa.angela.common.topology.Topology;
 
-import java.util.concurrent.Future;
+import java.util.concurrent.ExecutionException;
 
 import static com.terracottatech.qa.angela.common.topology.Version.version;
 
@@ -19,14 +18,15 @@ import static com.terracottatech.qa.angela.common.topology.Version.version;
  * @author Aurelien Broszniowski
  */
 
-public class InstallTest {
+public class MultistripesTest {
 
   @Test
-  public void testRemoteInstall() throws Exception {
+  public void test2Stripes() throws InterruptedException, ExecutionException {
 
-    TcConfig tcConfig = new TcConfig(version("10.1.0-SNAPSHOT"), "/terracotta/10/tc-config-a.xml");
+    TcConfig tcConfig1 = new TcConfig(version("10.1.0-SNAPSHOT"), "/terracotta/10/tc-config-multistripes1.xml");
+    TcConfig tcConfig2 = new TcConfig(version("10.1.0-SNAPSHOT"), "/terracotta/10/tc-config-multistripes2.xml");
 
-    Topology topology = new Topology("1", Distribution.distribution(version("10.1.0-SNAPSHOT"), PackageType.KIT, LicenseType.TC_DB), tcConfig);
+    Topology topology = new Topology("1", Distribution.distribution(version("10.1.0-SNAPSHOT"), PackageType.KIT, LicenseType.TC_DB), tcConfig1, tcConfig2);
     License license = new License("/terracotta/10/TerracottaDB101_license.xml");
 
     try (TsaControl control = new TsaControl(topology, license)) {
@@ -37,14 +37,10 @@ public class InstallTest {
       System.out.println("---> Wait for 3 sec");
       Thread.sleep(3000);
 
-      Future<Void> f = control.clientControl().submit("localhost", () -> System.out.println("hello dudes 5"));
-      f.get();
 
       System.out.println("---> Stop");
       control.stopAll();
+
     }
-
-
   }
-
 }
---
+++ integration-test/src/test/java/com/terracottatech/qa/angela/TcDBTest.java
@@ -0,0 +1,63 @@
+package com.terracottatech.qa.angela;
+
+import org.junit.Test;
+
+import com.terracottatech.qa.angela.client.TsaControl;
+import com.terracottatech.qa.angela.common.distribution.Distribution;
+import com.terracottatech.qa.angela.common.tcconfig.License;
+import com.terracottatech.qa.angela.common.tcconfig.TcConfig;
+import com.terracottatech.qa.angela.common.topology.LicenseType;
+import com.terracottatech.qa.angela.common.topology.PackageType;
+import com.terracottatech.qa.angela.common.topology.Topology;
+import com.terracottatech.store.Dataset;
+import com.terracottatech.store.Type;
+import com.terracottatech.store.configuration.DatasetConfigurationBuilder;
+import com.terracottatech.store.manager.DatasetManager;
+
+import java.net.URI;
+import java.util.concurrent.Future;
+
+import static com.terracottatech.qa.angela.common.topology.Version.version;
+
+/**
+ * @author Aurelien Broszniowski
+ */
+
+public class TcDBTest {
+  @Test
+  public void testConnection() throws Exception {
+    TcConfig tcConfig = new TcConfig(version("10.2.0-SNAPSHOT"), "/terracotta/10/tc-config-a.xml");
+
+    Topology topology = new Topology("1", Distribution.distribution(version("10.2.0-SNAPSHOT"), PackageType.KIT, LicenseType.TC_DB), tcConfig);
+    License license = new License("/terracotta/10/TerracottaDB101_license.xml");
+
+    try (TsaControl control = new TsaControl(topology, license)) {
+      control.installAll();
+      control.startAll();
+      control.licenseAll();
+
+      System.out.println("---> Wait for 3 sec");
+      Thread.sleep(3000);
+
+      Future<Void> f = control.clientControl().submit("localhost", () -> {
+            try {
+              DatasetManager datasetManager = DatasetManager.clustered(URI.create("terracotta://localhost:9510")).build();
+              DatasetConfigurationBuilder builder = datasetManager.datasetConfiguration()
+                  .offheap("primary-server-resource");
+              Dataset<String> dataset = datasetManager.createDataset("MyDataset", Type.STRING, builder.build());
+
+              // We managed to connect
+
+              dataset.close();
+            } catch (Exception e) {
+              throw new RuntimeException(e);
+            }
+          }
+      );
+      f.get();
+
+      System.out.println("---> Stop");
+      control.stopAll();
+    }
+  }
+}
