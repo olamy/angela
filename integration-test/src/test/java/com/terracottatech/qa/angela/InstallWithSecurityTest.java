d06efa32c3bdf0b90e7e38f697e0f6ac048edaaf
Ludovic Orban <lorban@bitronix.be>
2018-02-02 17:41:12 +0100

    Ignore unstable test for now

--- integration-test/src/test/java/com/terracottatech/qa/angela/InstallWithSecurityTest.java
+++ integration-test/src/test/java/com/terracottatech/qa/angela/InstallWithSecurityTest.java
@@ -1,5 +1,6 @@
 package com.terracottatech.qa.angela;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.terracottatech.qa.angela.client.ClusterFactory;
@@ -23,6 +24,7 @@ import static com.terracottatech.qa.angela.common.topology.Version.version;
 public class InstallWithSecurityTest {
 
   @Test
+  @Ignore
   public void testLocalInstallWithSecurity() throws Exception {
     Topology topology =
         new Topology(distribution(version("10.2.0.0.224"),
@@ -43,6 +45,7 @@ public class InstallWithSecurityTest {
   }
 
   @Test
+  @Ignore
   public void testLocalInstallAPWithSecurity() throws Exception {
     Topology topology =
         new Topology(distribution(version("10.2.0.0.224"),
