1fbf49b24f492c0df4274a144b676b53be99ca98
Venkata Sairam Madduri <VenkataSairam.Madduri@softwareag.com>
2018-02-07 17:45:27 +0530

    Add multi-stripe security test

---
+++ integration-test/src/test/java/com/terracottatech/qa/angela/MultiStripeWithSecurityTest.java
@@ -0,0 +1,44 @@
+package com.terracottatech.qa.angela;
+
+import org.junit.Test;
+
+import com.terracottatech.qa.angela.client.ClusterFactory;
+import com.terracottatech.qa.angela.client.Tsa;
+import com.terracottatech.qa.angela.common.tcconfig.License;
+import com.terracottatech.qa.angela.common.tcconfig.SecureTcConfig;
+import com.terracottatech.qa.angela.common.tcconfig.ServerSymbolicName;
+import com.terracottatech.qa.angela.common.topology.LicenseType;
+import com.terracottatech.qa.angela.common.topology.PackageType;
+import com.terracottatech.qa.angela.common.topology.Topology;
+
+import static com.terracottatech.qa.angela.common.distribution.Distribution.distribution;
+import static com.terracottatech.qa.angela.common.tcconfig.NamedSecurityRootDirectory.withSecurityFor;
+import static com.terracottatech.qa.angela.common.tcconfig.SecureTcConfig.secureTcConfig;
+import static com.terracottatech.qa.angela.common.tcconfig.SecurityRootDirectory.securityRootDirectory;
+import static com.terracottatech.qa.angela.common.tcconfig.TcConfig.tcConfig;
+import static com.terracottatech.qa.angela.common.topology.Version.version;
+
+public class MultiStripeWithSecurityTest {
+
+  @Test
+  public void test2StripesWithSecurity() throws Exception {
+    Topology topology =
+        new Topology(distribution(version("10.2.0.0.224"), PackageType.KIT, LicenseType.TC_DB),
+                     secureTcConfig(version("10.2.0.0.224"),
+                                    getClass().getResource("/terracotta/10/tc-config-multistripes1-with-security.xml"),
+                                    withSecurityFor(new ServerSymbolicName("Server1-1"),
+                                                    securityRootDirectory(getClass().getResource("/terracotta/10/security")))),
+                     secureTcConfig(version("10.2.0.0.224"),
+                                    getClass().getResource("/terracotta/10/tc-config-multistripes2-with-security.xml"),
+                                    withSecurityFor(new ServerSymbolicName("Server1-2"),
+                                                    securityRootDirectory(getClass().getResource("/terracotta/10/security")))));
+    License license = new License(getClass().getResource("/terracotta/10/TerracottaDB101_license.xml"));
+
+    try (ClusterFactory factory = new ClusterFactory("MultiStripeWithSecurityTest::test2StripesWithSecurity")) {
+      Tsa tsa = factory.tsa(topology, license);
+      tsa.installAll();
+      tsa.startAll();
+      tsa.licenseAll(securityRootDirectory(getClass().getResource("/terracotta/10/security")));
+    }
+  }
+}
